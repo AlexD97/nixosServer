2871676348cc79c117903970628ee7ab03cdb2b8
Alexander Dinges <alexding97@gmail.com>
2023-02-11 23:17:01 +0100

    Alacritty theme

--- desktop/applications/default.nix
+++ desktop/applications/default.nix
@@ -2,4 +2,6 @@
 {
   xdg.configFile."sioyek/keys_user.config".source = ./sioyek_keys_user.config;
   xdg.configFile."sioyek/prefs_user.config".source = ./sioyek_prefs_user.config;
+  xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
+  xdg.configFile."alacritty/themes/AtomOneLight.conf".source = ./alacrittyAtomOneLight.conf;
 }
