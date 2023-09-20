a334bdce3f2b880635ee30f343ebf21495361bfc
Alexander Dinges <alexding97@gmail.com>
2023-09-20 17:32:58 +0200

    last before migration

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,19 @@ in
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "de_DE.utf8";
+  i18n.defaultLocale = "de_DE.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -78,7 +90,7 @@ in
   ];
 
   # Configure console keymap
-  console.keyMap = "de";
+  console.keyMap = "neo";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -162,6 +174,9 @@ in
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFEuNFhwtWarRtxgMUx3HPshk0Aj/yOqwBz/YdEQCP17 alexander"
     ];
   };
+
+  # Automatic login
+  services.getty.autologinUser = "${user}";
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
--- server/ntfy.nix
+++ server/ntfy.nix
@@ -2,10 +2,13 @@
 let
 
 in {
-  services.ntfy = {
+  services.ntfy-sh = {
     enable = true;
     settings = {
       listen-http = ":8123";
+      base-url = "https://ntfy.alexanderdinges.de";
+#      cache-file = "/home/alexander/Downloads/ntfy_cache.db";
+      cache-duration = "48h";
     };
   };
 }
