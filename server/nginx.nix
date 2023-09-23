f1c8ce78b929153e0c39b29850c861902427aca4
Alexander Dinges <alexding97@gmail.com>
2023-09-23 22:13:45 +0200

    fix too large chunk size immich

--- server/nginx.nix
+++ server/nginx.nix
@@ -8,7 +8,12 @@
       "photos.alexanderdinges.de" = {
         forceSSL = true;
         useACMEHost = "alexanderdinges.de";
-        locations."/".proxyPass = "http://127.0.0.1:2283";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:2283";
+          extraConfig = ''
+            client_max_body_size 0;
+          '';
+        };
       };
       "syncthing.alexanderdinges.de" = {
         forceSSL = true;
